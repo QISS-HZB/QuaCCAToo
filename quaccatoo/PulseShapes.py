4723889093a524541173a2388ca93a102edab373
Anmol Singh <anmol.singh@helmholtz-berlin.de>
2024-09-10 16:57:42 +0200

    changed param tmid to t_mid for snake_case consistency
    fix docstring to numpy format

--- quaccatoo/PulseShapes.py
+++ quaccatoo/PulseShapes.py
@@ -6,31 +6,40 @@ This module contains pulse shape functions to be used in the PulsedSim classes a
 
 import numpy as np
 
+
 def square_pulse(t, **pulse_params):
     """
     Square pulse envelope modulation
 
     Parameters
     ----------
-    - t (float): time parameter
-    - f_pulse (float): frequency of the pulse
-    - phi_t (float): phase of the pulse
+    t : float
+        time parameter
+    f_pulse : float
+        frequency of the pulse
+    phi_t : float
+        phase of the pulse
     """
     return np.cos(pulse_params['f_pulse']*t + pulse_params['phi_t'])
+
 
 def gaussian_pulse(t, **pulse_params):
     """
     Gaussian pulse envelope modulation
-    
+
     Parameters
     ----------
-    - t (float): time parameter
-    - tmid (float): middle point of the pulse
-    - sigma (float): width of the pulse
-    - f_pulse (float): frequency of the pulse
-    - phi_t (float): phase of the pulse
+    t : float
+        time parameter
+    t_mid : float
+        middle point of the pulse
+    sigma : float
+        width of the pulse
+    f_pulse float): frequency of the pulse
+    phi_t (float): phase of the pulse
     """
-    return np.exp(-((t - pulse_params['tmid'])**2)/(2*pulse_params['sigma']**2))*np.cos(pulse_params['f_pulse']*t + pulse_params['phi_t'])
+    return np.exp(-((t - pulse_params['t_mid'])**2)/(2*pulse_params['sigma']**2))*np.cos(pulse_params['f_pulse']*t + pulse_params['phi_t'])
+
 
 def lorentzian_pulse(t, **pulse_params):
     """
@@ -38,10 +47,15 @@ def lorentzian_pulse(t, **pulse_params):
 
     Parameters
     ----------
-    - t (float): time parameter
-    - tmid (float): middle point of the pulse
-    - gamma (float): width of the pulse
-    - f_pulse (float): frequency of the pulse
-    - phi_t (float): phase of the pulse
+    t : float
+        time parameter
+    t_mid : float
+        middle point of the pulse
+    gamma : float
+        width of the pulse
+    f_pulse : float
+        frequency of the pulse
+    phi_t : float
+        phase of the pulse
     """
-    return 1/(1 + ((t - pulse_params['tmid'])/['gamma'])**2)*np.cos(pulse_params['f_pulse']*t+ pulse_params['phi_t'])
+    return 1/(1 + ((t - pulse_params['t_mid'])/['gamma'])**2)*np.cos(pulse_params['f_pulse']*t+ pulse_params['phi_t'])
