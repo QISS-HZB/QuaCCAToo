3892c3e032759e8065b99885213253b3e55bd24e
Anmol Singh <anmol.singh@helmholtz-berlin.de>
2024-09-12 15:03:40 +0200

    can now install the package
    using pip install .

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QuaCCAToo',
+    version='0.1',
+    description='Quantum Color Centres Analysis Toolbox',
+    url='https://github.com/QISS-HZB/QuaCCAToo',
+    author='Lucas Tsunaki, Anmol Singh, Sergei Trofimov',
+    author_email='lucas.tsunaki@helmholt-berlin.de, anmol.singh@helmholtz-berlin.de, sergei.trofimov@helmholtz-berlin.de',
+    packages=find_packages(),
+    install_requires=['numpy', 'matplotlib', 'scipy', 'qutip'],
+    python_requires='>=3.9',
+    package_dir={'': '.'},
+)
