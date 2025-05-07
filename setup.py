f4b8a3f65ae648d6642b7da89b545cd54703a0e7
Anmol Singh <anmol.singh@helmholtz-berlin.de>
2025-05-07 15:39:08 +0200

    tag: 0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='QuaCCAToo',
-    version='0.4',
+    version='0.5',
     description='Quantum Color Centers Analysis Toolbox',
     url='https://github.com/QISS-HZB/QuaCCAToo',
     author='Lucas Tsunaki, Anmol Singh, Sergei Trofimov',
