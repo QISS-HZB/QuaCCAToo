2df98f308b77102fba78d17728b66e9d0b1ad524
Anmol Singh <anmol.singh@helmholtz-berlin.de>
2025-07-14 12:24:07 +0200

    tag: 0.5.3
    qutip limit lifted since upstream seems to have released the fix
    ref: 5cde0a8

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='QuaCCAToo',
-    version='0.5.1',
+    version='0.5.3',
     description='Quantum Color Centers Analysis Toolbox',
     url='https://github.com/QISS-HZB/QuaCCAToo',
     author='Lucas Tsunaki, Anmol Singh, Sergei Trofimov',
     author_email='lucas.tsunaki@helmholtz-berlin.de, anmol.singh@helmholtz-berlin.de, sergei.trofimov@helmholtz-berlin.de',
     packages=find_packages(),
-    install_requires=['numpy', 'matplotlib', 'scipy', 'qutip<5.1', 'lmfit'],
+    install_requires=['numpy', 'matplotlib', 'scipy', 'qutip', 'lmfit'],
     python_requires='>=3.9',
     package_dir={'': '.'},
 )
